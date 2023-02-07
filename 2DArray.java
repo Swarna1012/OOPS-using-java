8af796161441f8eeb8747f1c0f16d13889af1a8c
Swarna1012 <124652782+Swarna1012@users.noreply.github.com>
2023-02-07 10:25:46 +0530

    Two dimension array

---
+++ 2DArray.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class ArrayMulti
+{
+public static void main(String args[])
+{
+	int a[][]={{1,2,3},{4,5},{6,7,8,9}};
+	
+			System.out.print(a[0].length);
+		    System.out.print(a[1].length);
+}
+}
+	
